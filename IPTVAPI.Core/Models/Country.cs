7a0d3fb4e05d130958f03786023f2911828280e5
Manojbabu <83714923+manusoft@users.noreply.github.com>
2023-06-17 02:03:34 +0400

    Feature save API's by countries (#2)
    * Core service updated.
    
    * Add save files by country function.

---
+++ IPTVAPI.Core/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IPTVAPI.Core.Models;
+
+public class Country
+{
+    [JsonPropertyName("code")]
+    public string? Code { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("languages")]
+    public List<string>? Languages { get; set; }
+
+    [JsonPropertyName("flag")]
+    public string? Flag { get; set; }
+}
--- IPTVAPI.Core/Services/FetchService.cs
+++ IPTVAPI.Core/Services/FetchService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using IPTVAPI.Core.Constants;
 using IPTVAPI.Core.Models;
 using Newtonsoft.Json;
@@ -7,6 +8,24 @@ namespace IPTVAPI.Core.Services;
 
 public class FetchService
 {
+    public async Task<List<Country>> GetCountriesLocalAsync()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = "IPTVAPI.Core.Resources.Raws.countries.json";
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        var countryList = JsonConvert.DeserializeObject<List<Country>>(contents);
+
+        if (countryList != null)
+        {
+            return countryList;
+        }
+
+        return null;
+    }
+
     public async Task<List<OnlineChannel>> GetChannelsAsync()
     {
         try
